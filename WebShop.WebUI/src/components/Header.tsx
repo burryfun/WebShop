9e11a13a1640bc558aa50eb22c4d7dedff0fe00e
burryfun <kiril0108@gmail.com>
2022-08-06 23:21:41 +0600

    update Header

--- WebShop.WebUI/src/components/Header.tsx
+++ WebShop.WebUI/src/components/Header.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import ss_logo from '../assets/ss_logo.svg'
-import Modal from '../Modals/LoginFormModal';
 import LoginFormModal from '../Modals/LoginFormModal';
 
 const Header = () => {
