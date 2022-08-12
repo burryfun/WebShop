0f66c012ab93cb139a2fafb7e0974d6019d9f72e
burryfun <kiril0108@gmail.com>
2022-08-12 17:26:38 +0600

    add cart and order models

---
+++ WebShop.Domain/Cart.cs
@@ -0,0 +1,10 @@
+﻿
+namespace WebShop.Domain
+{
+    public class Cart
+    {
+        public Guid Id { get; set; }
+        public Guid AccountId { get; set; }
+        public List<Smartphone>? Smartphones { get; set; }
+    }
+}
---
+++ WebShop.Domain/Order.cs
@@ -0,0 +1,13 @@
+﻿
+namespace WebShop.Domain
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public Account Account { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public DateTime Created { get; set; }
+        public List<Smartphone> Smartphones { get; set; }
+    }
+}
