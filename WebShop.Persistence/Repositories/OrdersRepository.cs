13f8a02dbaba69d6c43efccda01d248d8724a769
burryfun <kiril0108@gmail.com>
2022-08-12 17:28:02 +0600

    add order repository

---
+++ WebShop.Persistence/Repositories/OrdersRepository.cs
@@ -0,0 +1,60 @@
+﻿using WebShop.Domain;
+
+namespace WebShop.Persistence.Repositories
+{
+    public class OrdersRepository : IRepository<Order>
+    {
+
+        private readonly WebShopContext _context;
+
+        public OrdersRepository(WebShopContext context)
+        {
+            _context = context;
+        }
+
+        public Order Create(Order order)
+        {
+            if (order == null)
+            {
+                throw new ArgumentNullException(nameof(order));
+            }
+
+            _context.orders.Add(order);
+            _context.SaveChanges();
+            return order;
+        }
+
+        public Order GetById(Guid id)
+        {
+            var order = _context.orders.FirstOrDefault(x => x.Id == id);
+            return order;
+        }
+
+        public bool Delete(Guid id)
+        {
+            var order = GetById(id);
+
+            if (order != null)
+            {
+                _context.orders.Remove(order);
+                _context.SaveChanges();
+                return true;
+            }
+
+            return false;
+        }
+
+
+        public IEnumerable<Order> GetAll()
+        {
+            return _context.orders;
+        }
+
+        
+
+        public Order Update(Order order)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
