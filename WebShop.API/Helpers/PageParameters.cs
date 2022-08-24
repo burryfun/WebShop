63087e33da231395fdad62f325ee710eb4a5869b
burryfun <kiril0108@gmail.com>
2022-08-25 01:02:09 +0600

    add pagination helper

---
+++ WebShop.API/Helpers/PageParameters.cs
@@ -0,0 +1,19 @@
+﻿namespace WebShop.API.Helpers
+{
+    public class PageParameters
+    {
+        const int maxPageSize = 5;
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public PageParameters()
+        {
+            PageNumber = 1;
+            PageSize = maxPageSize;
+        }
+        public PageParameters(int pageNumber, int pageSize)
+        {
+            PageNumber = pageNumber < 1 ? 1 : pageNumber;
+            PageSize = pageSize > maxPageSize ? maxPageSize : pageSize;
+        }
+    }
+}
