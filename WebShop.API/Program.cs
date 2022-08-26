7189bffaf10b0c4c35659f015120c79211adbf14
burryfun <kiril0108@gmail.com>
2022-08-27 00:51:54 +0600

    add exposed header <x-pagination> to cors

--- WebShop.API/Program.cs
+++ WebShop.API/Program.cs
@@ -18,7 +18,8 @@ builder.Services.AddCors(options =>
     options.AddPolicy(MyAllowSpecificOrigins,
                      policy =>
                      {
-                         policy.WithOrigins("http://localhost:3000");
+                         policy.WithOrigins("http://localhost:3000")
+                         .WithExposedHeaders("X-Pagination");
                          policy.WithMethods("GET", "POST", "PUT", "DELETE");
                          policy.AllowAnyHeader();
                          policy.AllowCredentials();
