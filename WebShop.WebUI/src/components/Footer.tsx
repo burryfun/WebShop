923e93376ac2d6101bf0d3b4ab4861703d83d9e9
burryfun <kiril0108@gmail.com>
2022-09-16 16:07:03 +0600

    frontend: add footer

---
+++ WebShop.WebUI/src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className='mt-10'>
+      <div className='h-40 bg-green'>
+
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
